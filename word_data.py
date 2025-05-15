8a2b501cc65b66b50b0fe302fa16635dabced99d
evelinabruzus <evelina.bruzus@gmail.com>
2025-05-15 16:54:43 +0300

    Update word_data.py

--- word_data.py
+++ word_data.py
@@ -1,1 +1,8 @@
 import random
+
+class WordList:
+    def __init__ (self):
+        self.words = ["skola", "saule", "draugi", "dators", "grāmata", "zirgs", "futbols", "grīda", "maize", "latviešu valoda"]
+
+    def get_random_word(self):
+      return random.choice(self.words)
